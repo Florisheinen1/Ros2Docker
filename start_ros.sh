95e88e52ee629bc78a08b02c45985213cbe92321
Floris Heinen <florisheinen1@gmail.com>
2025-02-13 04:01:32 +0100

    Pass optional webcam device to docker

--- start_ros.sh
+++ start_ros.sh
@@ -2,5 +2,14 @@
 echo Building image...
 docker build -t asdfr-ros-vnc -f Dockerfile.ros_vnc .
 
+# This passes the webcam file to the docker container. Leave empty if it gives trouble
+DEVICE_OPTION="--device=/dev/video0:/dev/video0"
+# DEVICE_OPTION=""
+
 echo Running container...
-docker run -it -p 5901:5901 --rm -v ./workspace:/root/ros_ws asdfr-ros-vnc
+docker run \
+    $DEVICE_OPTION \
+    -it \
+    -p 5901:5901 \
+    --rm \
+    -v ./workspace:/root/ros_ws asdfr-ros-vnc
